f0a944f3defae7cd9b510ea1eca4cba309ac5190
Domantas Andrijauskas <domantas.andrijauskas@kilo.health>
2022-05-09 19:43:18 +0300

    update gitignore

--- dist/routes/deviceToken.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const deviceTokenController = require('../controllers/deviceToken.controller');
-const validateFirebaseIdToken = require('../middleware/firebaseAuth');
-const deviceTokenRouter = express.Router();
-deviceTokenRouter.use(validateFirebaseIdToken);
-deviceTokenRouter.post('/', deviceTokenController.saveDeviceToken);
-module.exports = deviceTokenRouter;
